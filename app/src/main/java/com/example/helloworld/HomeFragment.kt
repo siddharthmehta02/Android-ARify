fa37a40f4d7055c26e38531c4761e60465b39697
NischayaSharma <nischaya1703@gmail.com>
2021-05-31 09:06:45 +0530

    AR Integrated

--- app/src/main/java/com/example/helloworld/HomeFragment.kt
+++ app/src/main/java/com/example/helloworld/HomeFragment.kt
@@ -5,27 +5,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+//import android.app.AlertDialog
+//import android.view.MotionEvent
+//import androidx.appcompat.app.AppCompatActivity
+//import com.google.ar.core.Anchor
+//import com.google.ar.core.HitResult
+//import com.google.ar.core.Plane
+//import com.google.ar.sceneform.AnchorNode
+//import com.google.ar.sceneform.rendering.ModelRenderable
+//import com.google.ar.sceneform.ux.ArFragment
+//import com.google.ar.sceneform.ux.TransformableNode
+//import java.util.function.Consumer
+//import java.util.function.Function
 
 
+
+//class HomeFragment : AppCompatActivity() {
 class HomeFragment : Fragment() {
-
+//    private var arCam: ArFragment? = null
+//    private var clickNo = 0
+//
+//    private fun addModel(anchor: Anchor, modelRenderable: ModelRenderable) {
+//
+//        // Creating a AnchorNode with a specific anchor
+//        val anchorNode = AnchorNode(anchor)
+//
+//        // attaching the anchorNode with the ArFragment
+//        anchorNode.setParent(arCam!!.arSceneView.scene)
+//        val transform = TransformableNode(arCam!!.transformationSystem)
+//
+//        // attaching the anchorNode with the TransformableNode
+//        transform.setParent(anchorNode)
+//
+//        // attaching the 3d model with the TransformableNode that is
+//        // already attached with the node
+//        transform.renderable = modelRenderable
+//        transform.select()
+//    }
 
     override fun onCreateView(
+//    fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+//    ) {
+    // Inflate the layout for this fragment
+//        arCam = fragmentManager.findFragmentById(R.id.arCameraArea) as ArFragment
+//        arCam!!.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane?, motionEvent: MotionEvent? ->
+//            clickNo++
+//            if (clickNo == 1) {
+//                val anchor = hitResult.createAnchor()
+//                ModelRenderable.builder()
+//                    .setSource(this, R.raw.syringe_aot)
+//                    .setIsFilamentGltf(true)
+//                    .build()
+//                    .thenAccept(Consumer<ModelRenderable> { modelRenderable: ModelRenderable? -> addModel(anchor, modelRenderable!!) })
+//                    .exceptionally(Function<Throwable, Void?> { throwable: Throwable ->
+//                        val builder = AlertDialog.Builder(this)
+//                        builder.setMessage("Somthing is not right" + throwable.message).show()
+//                        null
+//                    })
+//            }
+//        }
+            return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
-
         @JvmStatic
         fun newInstance() =
             HomeFragment().apply {
                 arguments = Bundle().apply { }
             }
-
-
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.helloworld
 
-import android.animation.LayoutTransition
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatDialogFragment
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,26 +19,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation:MeowBottomNavigation = findViewById(R.id.bottomNavBar)
+        if (this.checkSystemSupport(this)) {
 
-        addFragment(HomeFragment.newInstance())
-        bottomNavigation.show(1)
-        bottomNavigation.add(MeowBottomNavigation.Model(0, R.drawable.ic_info))
-        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_camera))
-        bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_search))
+            val bottomNavigation: MeowBottomNavigation = findViewById(R.id.bottomNavBar)
 
-        bottomNavigation.setOnClickMenuListener {
-            when(it.id){
-                0->{
-                    replaceFragment(AboutFragment.newInstance())
-                }
-                1->{
-                    replaceFragment(HomeFragment.newInstance())
+            addFragment(HomeFragment.newInstance())
+            bottomNavigation.show(1)
+            bottomNavigation.add(MeowBottomNavigation.Model(0, R.drawable.ic_info))
+            bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_camera))
+            bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_search))
 
-                }
-                2->{
-                    replaceFragment(BrowseFragment.newInstance())
+            bottomNavigation.setOnClickMenuListener {
+                when (it.id) {
+                    0 -> {
+                        replaceFragment(AboutFragment.newInstance())
+                    }
+                    1 -> {
+                        replaceFragment(HomeFragment.newInstance())
 
+                    }
+                    2 -> {
+                        replaceFragment(BrowseFragment.newInstance())
+
+                    }
                 }
             }
         }
@@ -42,6 +49,38 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun checkSystemSupport(activity: Activity): Boolean {
+
+        // checking whether the API version of the running Android >= 24
+        // that means Android Nougat 7.0
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val openGlVersion =
+                (Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)) as ActivityManager).deviceConfigurationInfo.glEsVersion
+
+            // checking whether the OpenGL version >= 3.0
+            if (openGlVersion.toDouble() >= 3.0) {
+                true
+            } else {
+                Toast.makeText(
+                    activity,
+                    "App needs OpenGl Version 3.0 or later",
+                    Toast.LENGTH_SHORT
+                ).show()
+                activity.finish()
+                false
+            }
+        } else {
+            Toast.makeText(
+                activity,
+                "App does not support required Build Version",
+                Toast.LENGTH_SHORT
+            ).show()
+            activity.finish()
+            false
+        }
+    }
+
 
     private fun replaceFragment(fragment: Fragment){
         val fragmentTransition=supportFragmentManager.beginTransaction()
